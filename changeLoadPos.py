a6fbae7ace040920c1acb0115bf3897d1dad92c3
Gavin Day <mngad@leeds.ac.uk>
2016-09-05 14:57:25 +0100

    commented

--- changeLoadPos.py
+++ changeLoadPos.py
@@ -2,32 +2,25 @@
 import csv
 import copy
 
-f = open('loadPosition.csv')
+f = open('loadPosition.csv') #opens initial loadPosition.csv
 csv_f = csv.reader(f)
-changedRow = [0,0,0,0]
-o = open('loadPositionEdit.csv','w')
+o = open('loadPositionEdit.csv','w') #opens new loadPosition.csv
 csv_o = csv.writer(o)
 for row in csv_f:
-      changedRow = [0,0,0,0]
-      #print(row)
       changedRow = row.copy()
       for i in range(0,5):
           if i > 0:
-              #print('row = ' + str(row))
-
-              changedRow = row.copy()
-
+              changedRow = row.copy() #Resets changedRow
               changedRow[0] = row[0]+str(i)
               if i == 1:
-                  changedRow[2] = int(row[2])-1
+                  changedRow[2] = int(row[2])-1 #North
               if i == 2:
-                  changedRow[1] = int(row[1])+1
+                  changedRow[1] = int(row[1])+1 #East
               if i == 3:
-                  changedRow[2] = int(row[2])+1
+                  changedRow[2] = int(row[2])+1 #South
               if i == 4:
-                  changedRow[1] = int(row[1])-1
+                  changedRow[1] = int(row[1])-1 #West
           csv_o.writerow(changedRow)
-print('----------------------------------')
 o.close()
 o2 = open('loadPositionEdit.csv')
 csv_o2 = csv.reader(o2)
