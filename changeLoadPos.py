f389cfaf3b5235623d40168d9f147b608ced1a5e
Gavin Day <mngad@leeds.ac.uk>
2016-09-05 14:53:43 +0100

    Working with 4 - NESW extra positions for loadpos.csv

--- changeLoadPos.py
+++ changeLoadPos.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3
 import csv
+import copy
 
 f = open('loadPosition.csv')
 csv_f = csv.reader(f)
-
+changedRow = [0,0,0,0]
 o = open('loadPositionEdit.csv','w')
 csv_o = csv.writer(o)
 for row in csv_f:
-      print row
-      changedRow = row
+      changedRow = [0,0,0,0]
+      #print(row)
+      changedRow = row.copy()
       for i in range(0,5):
-          for x in range(0,len(changedRow)):
-              changedRow[x] = row[x]
-              print changedRow[x]
           if i > 0:
+              #print('row = ' + str(row))
+
+              changedRow = row.copy()
+
               changedRow[0] = row[0]+str(i)
               if i == 1:
                   changedRow[2] = int(row[2])-1
@@ -23,9 +27,9 @@ for row in csv_f:
               if i == 4:
                   changedRow[1] = int(row[1])-1
           csv_o.writerow(changedRow)
-print '----------------------------------'
+print('----------------------------------')
 o.close()
 o2 = open('loadPositionEdit.csv')
 csv_o2 = csv.reader(o2)
 for row in csv_o2:
-      print row
+      print(row)
