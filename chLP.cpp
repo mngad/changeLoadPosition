ad08ad679c2b2f0876035465e676e1150cf70c7c
Gavin Day <mngad@leeds.ac.uk>
2018-03-13 19:12:25 +0000

    formatting

--- chLP.cpp
+++ chLP.cpp
@@ -7,17 +7,12 @@
 namespace fs = std::experimental::filesystem;
 
 int main(){
-
 	std::ofstream outfile;
 	outfile.open("out.csv");
 	io::CSVReader<4> in("loadPosition.csv");
 	std::string name; int posX; int posY; int posZ;
 	while(in.read_row(name, posX, posY, posZ)){
-		//std::cout <<name<<" "<<posX<<" "<<posY<<" "<<posZ<< std::endl;
 		for(int i = 0; i<9;i++){
-			
-
-			//std::cout<<i<<std::endl;
 			if(i==0)
 				outfile << name<<", "<<posX<<", "<<posY<<", "<<posZ<<std::endl;	
 			if(i==1)
@@ -39,8 +34,6 @@ int main(){
 			if(i>=1){
 				std::string oldFname = name + ".inp";
 				std::string newFname = name + std::to_string(i) + ".inp";
-				//std::cout<<oldFname<<std::endl;
-				//std::cout<<newFname<<std::endl;
 				fs::copy(oldFname,newFname,fs::copy_options::overwrite_existing );
 			}
 		}
